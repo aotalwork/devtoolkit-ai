dd72cbeb2d7898942e63cadcefd7c636a86f4ee5
Arantzazu Otal <arantxaotal@gmail.com>
2026-07-30 11:46:07 +0200

    sprint 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get "dashboard/index"
   root "dashboard#index"
 
-  resources :tools, only: [:show]
+  resources :tools, only: [:show] do
+    post :analyze, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
