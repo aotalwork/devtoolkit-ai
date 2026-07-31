73550b606605ba1e33280c158fafa9d56f823b53
Arantxa Otal <arantxaotal@gmail.com>
2026-07-31 11:32:01 +0200

    sprint 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,17 @@
 Rails.application.routes.draw do
-  get "tools/show"
-  get "dashboard/index"
   root "dashboard#index"
+
+  resources :error_debugger, only: [:index, :create]
+  resources :test_generator, only: [:index, :create]
+  resources :commit_coach, only: [:index, :create]
+  resources :readme_generator, only: [:index, :create]
+  resources :pr_reviewer, only: [:index, :create]
+  resources :sql_optimizer, only: [:index, :create]
+  resources :code_explainer, only: [:index, :create]
 
   resources :tools, only: [:show] do
     post :analyze, on: :member
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
