a20175f18059150f5df4aaf4d363a9c58ac93a19
Arantxa Otal <arantxaotal@gmail.com>
2026-07-31 12:28:19 +0200

    sprint 3

--- app/services/ollama_client.rb
+++ app/services/ollama_client.rb
@@ -1,20 +1,32 @@
-require "net/http"
-require "json"
-
 class OllamaClient
-  URL = URI("http://localhost:11434/api/generate")
+
+  URL = "http://localhost:11434/api/generate"
 
   def self.generate(prompt)
-    response = Net::HTTP.post(
-      URL,
-      {
-        model: "llama3.2:3b",
-        prompt: prompt,
-        stream: false
-      }.to_json,
-      "Content-Type" => "application/json"
-    )
 
-    JSON.parse(response.body)["response"]
+    uri = URI(URL)
+
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.open_timeout = 10
+    http.read_timeout = 120
+
+    request = Net::HTTP::Post.new(uri)
+    request["Content-Type"] = "application/json"
+
+    request.body = {
+      model: "llama3.2:3b",
+      prompt: prompt,
+      stream: false
+    }.to_json
+
+    puts "🚀 Enviando a Ollama"
+
+    response = http.request(request)
+
+    puts "✅ Respuesta recibida"
+
+    JSON.parse(response.body)
+
   end
+
 end
