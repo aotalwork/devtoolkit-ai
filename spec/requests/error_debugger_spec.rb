4d6a59e0125daf4abb46f9fcc9c46fbdb05d0d02
Arantzazu Otal <arantxaotal@gmail.com>
2026-08-02 21:48:00 +0200

    Prepare for deployment

--- spec/requests/error_debugger_spec.rb
+++ spec/requests/error_debugger_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "ErrorDebuggers", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      error_debugger_index_path
+      get error_debugger_index_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /create" do
     it "returns http success" do
-      error_debugger_index_path
+      get error_debugger_index_path
       expect(response).to have_http_status(:success)
     end
   end
